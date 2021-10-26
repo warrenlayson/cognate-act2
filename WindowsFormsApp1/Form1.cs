fdde146cc557aa2c5c8a68554b7ddaffc6a9c833
BertGuiang <guiang.robert6364@gmail.com>
2021-10-26 19:22:17 +0800

    Revert "hatdog"
    This reverts commit dfdaa0f3d8405648ef6b2b00c53008009c21b58a.

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,10 +16,5 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-
-        private void label7_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
